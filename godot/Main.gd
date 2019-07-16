8ebe7201b3bc2921db1cc7444f048999c6fe07f5
Pablo Ruiz <diacritica@gmail.com>
2019-07-16 22:05:10 +0200

    changes to radio pngs and main game

--- godot/Main.gd
+++ godot/Main.gd
@@ -3,13 +3,30 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const ASTRO_SPEED = 5
+const ASTRO_SPEED = 100
 
-var astronaut
+var dude
+var meteor1
+var meteor2
+var meteor3
+var meteor4
+var meteor5
+
+var ml1list
+
+var flag
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	astronaut = get_node("astrodude")
+	ml1list = []
+	add_meteorline1(400,300)
+	add_meteorline1(500,300)
+	add_meteorline1(600,300)
+	add_meteorline1(700,300)
+	add_meteorline1(800,300)
+
+#	flagizq = get_node("flags").get_node("flagL")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -17,22 +34,19 @@ func _ready():
 
 func _process(delta):
 	process_player_input(delta)
+	#process_meteor1(delta)
+	
 
+func add_meteorline1(x, y):
+	var m = load("res://Meteor01.tscn").instance()
+	var ml1 = m.get_node("Sprite").duplicate()
+	ml1.position.x = x
+	ml1.position.y = y
+	ml1list.append(ml1)
+	add_child(ml1)
+
+func create_meteor1():
+	pass
 
 func process_player_input(delta):
-	if Input.is_action_pressed("ui_right"):
-		astronaut.dude.position.x += ASTRO_SPEED
-		if astronaut.dude.position.x >=500:
-			astronaut.dude.position.x = 500
-	elif Input.is_action_pressed("ui_left"):
-		astronaut.position.x -= ASTRO_SPEED 
-		if astronaut.position.x <=0:
-			astronaut.position.x = 0
-	elif Input.is_action_pressed("ui_up"):		
-		astronaut.position.y -= ASTRO_SPEED
-		if astronaut.position.y <= 0:
-			astronaut.position.y = 0
-	elif Input.is_action_pressed("ui_down"):		
-		astronaut.position.y += ASTRO_SPEED
-		if astronaut.position.y >= 300:
-			astronaut.position.y = 300
+	pass
