b3642dcc43ae51080cf3b9213548250253de5cbb
Pablo Ruiz <diacritica@gmail.com>
2019-07-17 17:28:50 +0200

    dude playing plus sound

--- godot/Dude.gd
+++ godot/Dude.gd
@@ -8,7 +8,7 @@ var pos = [0,0]
 var coord_sprites
 var startpos = [1,1]
 
-
+var deaddude = false
 
 func move(row,col,direction):
 	if direction == "jump":
@@ -54,41 +54,132 @@ func _ready():
 
 
 	d1_1.visible = true
-	coord_sprites = {[1,1]: d1_1, [1,2]: d1_2, [1,3]: d1_3}
+	coord_sprites = {[1,1]: d1_1, [1,2]: d1_2, [1,3]: d1_3, [1,4]: d1_4, [1,5]: d1_5, [1,6]: d1_6, [1,7]: d1_7,
+	[2,2]: d2_2, [2,3]: d2_3, [2,4]: d2_4, [2,5]: d2_5, [2,6]: d2_6, 
+	[3,2]: d3_2, [3,3]: d3_3, [3,4]: d3_4, [3,5]: d3_5, [3,6]: d3_6,
+	[4,2]: d4_2, [4,3]: d4_3, [4,4]: d4_4, [4,5]: d4_5, [4,6]: d4_6,
+	[5,2]: d5_2, [5,3]: d5_3, [5,4]: d5_4, [5,5]: d5_5, [5,6]: d5_6,}
 	
 
+func dead():
+	var dead = load("res://Dead.tscn").instance()
+	
+	dead.position.x = 42
+	dead.position.y = 26
+	get_node("dead").play()
+	deaddude = true
+	
 
-func go_right(startpos):
+func go_right(startpos, delta):
 	var startdude = coord_sprites[startpos]
 	var row = startpos[0]
 	var col = startpos[1]
-	var newdude = coord_sprites[[row,col+1]]
+	get_node("move").play()
+	if next_move_is_valid([row, col+1]):
+		var newdude = coord_sprites[[row,col+1]]
+		currentdeltaforgravity = 0
+		return [row,col+1]
+	else:
+		currentdeltaforgravity += delta
+		var newdude = coord_sprites[[row,col]]
+		return [row,col]
 	
-	return [row,col+1]
-	
-func go_left(startpos):
-	pass
+func go_left(startpos, delta):
+	var startdude = coord_sprites[startpos]
+	var row = startpos[0]
+	var col = startpos[1]
+	get_node("move").play()
+	if next_move_is_valid([row, col-1]):
+		var newdude = coord_sprites[[row,col-1]]
+		currentdeltaforgravity = 0
+		return [row,col-1]
+	else:
+		currentdeltaforgravity += delta
+
+		var newdude = coord_sprites[[row,col]]
+		return [row,col]
 	
 func jump(startpos):
-	pass	
+	var startdude = coord_sprites[startpos]
+	var row = startpos[0]
+	var col = startpos[1]
+	get_node("move").play()
+	if next_move_is_valid([row-1, col]):
+		var newdude = coord_sprites[[row-1,col]]
+		currentdeltaforgravity = 0
+		return [row-1,col]
+	else:
+		var newdude = coord_sprites[[row,col]]
+		return [row,col]
 	
 
 func go_down(startpos):	
-	pass
-	
-func _process(delta):
-	
-	if Input.is_action_just_released("ui_right"):
-		startpos = go_right(startpos)
-		print(startpos)
-	elif Input.is_action_just_released("ui_left"):
-		go_left(startpos)
-	elif Input.is_action_pressed("ui_accept"):
-		jump(startpos)
-	elif Input.is_action_pressed("ui_down"):
-		go_down(startpos)
-	
+	var startdude = coord_sprites[startpos]
+	var row = startpos[0]
+	var col = startpos[1]
+	get_node("move").play()
+	if next_move_is_valid([row+1, col]):
+		var newdude = coord_sprites[[row+1,col]]
+		currentdeltaforgravity = 0
+		return [row+1,col]
+	else:
+		dead()
+		var newdude = coord_sprites[[row,col]]
+		return [row,col]
 
+func draw_dude(pos):
+	var dude_to_draw = coord_sprites[pos]
+	dude_to_draw.visible = true
+
+func hide_dude(pos):
+	var dude_to_hide = coord_sprites[pos]
+	dude_to_hide.visible = false
+	
+func next_move_is_valid(newpos):
+	if newpos in coord_sprites.keys():
+		return true
+	else:
+		return false
+
+var maxdeltaforgravity = 1.5
+var currentdeltaforgravity = 0
+
+func _process(delta):
+	if not deaddude:
+		currentdeltaforgravity += delta
+		
+		if Input.is_action_just_released("ui_right"):
+			hide_dude(startpos)
+			startpos = go_right(startpos, delta)
+			print(startpos)
+			draw_dude(startpos)
+			
+		elif Input.is_action_just_released("ui_left"):
+			hide_dude(startpos)
+			startpos = go_left(startpos, delta)
+			print(startpos)
+			draw_dude(startpos)	
+
+		elif Input.is_action_just_released("ui_accept"):
+			hide_dude(startpos)
+			startpos = jump(startpos)
+			print(startpos)
+			draw_dude(startpos)
+			currentdeltaforgravity = 0
+		elif Input.is_action_just_released("ui_down"):
+			hide_dude(startpos)
+			startpos = go_down(startpos)
+			print(startpos)
+			draw_dude(startpos)
+			currentdeltaforgravity = 0
+		if currentdeltaforgravity >= maxdeltaforgravity:
+			hide_dude(startpos)
+			startpos = go_down(startpos)
+			print(startpos)
+			draw_dude(startpos)
+			currentdeltaforgravity = 0
+	else:
+		print("DEAD")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
