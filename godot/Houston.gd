4323ccb801a62b09d27904a68086fa4e9a131be3
EstherMoreno <esther.moreno@kaleidos.net>
2019-07-18 12:26:24 +0200

    pinta_lineas_houston_version1

--- godot/Houston.gd
+++ godot/Houston.gd
@@ -3,9 +3,12 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var counter
+var counter = 0
 var box
 var lever_up
+var line1
+var line2
+var line3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,11 +16,32 @@ func _ready():
 	box.visible = true
 	lever_up = get_node("leverup")
 	lever_up.visible = true
+	line1 = get_node("radio1")
+	line2 = get_node("radio2")
+	line3 = get_node("radio3")
 
 
 func _process(delta):
 	
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+	if Input.is_action_just_released("ui_up"):
+		if counter == 0:
+			line1.visible = true
+			counter = 1
+		elif counter == 1:
+			line2.visible = true
+			counter = 2
+		elif counter == 2: 
+			line3.visible = true
+			counter = 3
+
+	if Input.is_action_just_released("ui_down"):
+		if counter == 3:
+			line3.visible = false
+			counter = 2
+		elif counter == 2:
+			line2.visible = false
+			counter = 1
+		elif counter == 1:
+			line1.visible = false
+			counter = 0
